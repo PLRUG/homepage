1e486f54452fe1936841d9b744f3a87f5b86025a
Oskar Lakner <oskar@nerds.family>
2023-02-01 14:46:40 +0000

    Render on homepage live members count

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,8 @@
 class PagesController < ApplicationController
   def home
     contact_request = ContactRequest.new
+    discord_gateway = DiscordGateway.new
 
-    render :home, locals: { contact_request: contact_request }
+    render :home, locals: { contact_request: contact_request, discord_gateway: discord_gateway }
   end
 end
